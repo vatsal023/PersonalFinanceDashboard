2da0d58e2ade2bf9481715068c2c904e5362d5a1
vatsal023 <vatsalgarg504@gmail.com>
2025-10-27 17:33:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ const budgetRoutes = require('./routes/budgetRoutes.js');
 const manualExpenseRoutes = require('./routes/manualExpenseRoutes.js');
 const investmentRoutes = require("./routes/investmentRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const mutualFundRoutes = require("./routes/mutualFundsRoutes");
+const csvRoutes = require("./routes/csvRoutes");
+const startFinnhubServer = require('./finnhubserver');
+const finnhubRoutes = require("./routes/finnhubRoutes");
+const shareRoutes = require("./routes/shareRoutes");
+const yahooFinanceRoutes = require("./routes/yahooFinanceRoutes");
+const yahooFinance = require("./routes/yahooFinance");
+const metalRoutes = require("./routes/metalRoutes");
+const mfRoutes = require("./routes/mfRoutes");
+const bullionRoutes = require("./routes/bullionRoutes");
+// const amcRoutes = require("./routes/amc");
 // inside app.js
 
 // Load env vars
@@ -48,11 +60,24 @@ connectDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/plaid', plaidRoutes);
 app.use("/api/budgets", budgetRoutes);
-app.use("/api/manual-expenses", manualExpenseRoutes);
+app.use("/api/expenses", manualExpenseRoutes);
 app.use("/api/investments", investmentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+app.use("/api/income", incomeRoutes)
+app.use("/api/mutualfunds", mutualFundRoutes);
+app.use("/api/csv", csvRoutes);
+app.use("/api/finnhub", finnhubRoutes);
+app.use("/api/shares", shareRoutes);
+// app.use("/api/yahoo", yahooFinanceRoutes);
+app.use("/api/yahoo", yahooFinance);
+app.use("/api/metal", metalRoutes);
+app.use("/api/mf",mfRoutes);
 
-
+// app.use("/api/mutual-funds", amcRoutes);
+app.use("/api/mutual-funds",require("./routes/AMFI"));
+app.use("/api/bullions", bullionRoutes);
 // Start server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = startFinnhubServer(app); 
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
