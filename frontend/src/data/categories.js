36c7afc73ea368a7e84d1f6bd6df953fd5d1f4ff
Vatsal Garg <144517333+vatsal023@users.noreply.github.com>
2025-12-27 16:34:41 +0530

    feat: create Categories.js

---
+++ frontend/src/data/categories.js
@@ -0,0 +1,16 @@
+export const categories = [
+  "Groceries",
+  "Food items",
+  "Rent/EMI",
+  "Tution fees",
+  "Electricity Bill",
+  "Water Bill",
+  "Gas Bill",
+  "Maid fees",
+  "Tours and Travels",
+  "Entertainment",
+  "Personal Shopping",
+  "Home Appliances",
+  "Dining",
+  "Miscellaneous"
+];
